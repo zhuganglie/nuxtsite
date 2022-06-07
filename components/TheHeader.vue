ce5a817d19cf0597ead3f1f0e7e6106d6fb6d69a
zhuganglie <iwifer@gmail.com>
2022-06-07 10:40:59 +0800

    update

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,11 +1,12 @@
 <template>
-<div class="flex items-center justify-center ">
-    <h1 class="font-bold w-1/2 uppercase">strategy</h1>
-    <nav class="hiden md:flex gap-4 w-1/2">
-        <NuxtLink to="/" exactActiveClass="text-red-700">Home</NuxtLink>
-        <NuxtLink to="/about" exactActiveClass="text-red-700">About</NuxtLink>
-        <NuxtLink to="/articles" exactActiveClass="text-red-700">Blog</NuxtLink>
+<header class="flex items-center justify-center mb-16 bg-purple-100 px-6 sticky top-0 ">
+    <span class="text-xl font-bold w-1/2 uppercase">ST</span>
+
+    <nav class="hiden md:flex gap-6 w-1/2">
+        <NuxtLink to="/" class="font-semibold no-underline" exactActiveClass="text-red-700">Home</NuxtLink>
+        <NuxtLink to="/about" class="font-semibold no-underline" exactActiveClass="text-red-700">About</NuxtLink>
+        <NuxtLink to="/articles" class="font-semibold no-underline" exactActiveClass="text-red-700">Blog</NuxtLink>
     </nav>
-</div>
+</header>
 
 </template>
