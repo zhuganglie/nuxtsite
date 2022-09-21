d5b83dd86fac894afca866aff6fa16a0024586ff
zhuganglie <pyrrhonianpig@gmail.com>
2022-09-21 23:35:52 +0800

    update

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,5 +1,5 @@
 <template>
-<footer class="bg-zinc-100 sticky bottom-0 w-full py-2 text-center">
+<footer class="bg-zinc-100 bottom-0 w-full py-2 text-center">
     <span>&copy; 2020</span> -
     <span>{{new Date().getFullYear()}}</span>
 </footer>
