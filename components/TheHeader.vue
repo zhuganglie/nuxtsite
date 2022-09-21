54c153e23ac6d3182b2a93543f514ac320e2d5b7
zhuganglie <pyrrhonianpig@gmail.com>
2022-09-21 23:29:27 +0800

    update

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,62 +1,53 @@
 
 <template>
-  <header class="z-999 bg-zinc-100 top-0 sticky w-full flex justify-between items-start">
-    <router-link to="/" class="px-8 py-6 text-xl font-bold md:text-2xl no-underline">ST</router-link>
-    <nav @click="toggleNav" class="container px-8 py-6 flex flex-col-reverse  items-end md:flex-row justify-between md:items-center">
-     <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
-      <ul :class="showMenu ? 'flex' : 'hidden'" class="min-w-full list-none flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:m-0">
-        <li class=""><NuxtLink to="/" class="no-underline font-semibold hover:bg-zinc-200 px-2 py-1 uppercase" activeClass="text-red-700">Home</NuxtLink></li>
-        <li class=""><NuxtLink to="/about" class="no-underline font-semibold hover:bg-zinc-200 px-2 py-1 uppercase" activeClass="text-red-700">About</NuxtLink></li>
-        <li class=""><NuxtLink to="/blog" class="no-underline font-semibold hover:bg-zinc-200 px-2 py-1 uppercase" activeClass="text-red-700">Blog</NuxtLink></li>
-      </ul> 
+  <div class="z-999 bg-zinc-100 top-0 sticky w-full container">
+     <nav class=" px-8 py-6 flex justify-between">
+
+    <router-link to="/" class=" text-xl font-bold md:text-2xl no-underline">ST</router-link>
+   
+      <div :class="showMenu ? 'flex flex-col' : 'hidden'" class=" list-none mt-8 space-y-4  md:space-y-0 md:flex md:items-center md:space-x-10 md:m-0">
+        <NuxtLink to="/" class="no-underline font-semibold hover:bg-zinc-200 px-2 py-1 uppercase" activeClass="text-red-700">Home</NuxtLink>
+        <NuxtLink to="/about" class="no-underline font-semibold hover:bg-zinc-200 px-2 py-1 uppercase" activeClass="text-red-700">About</NuxtLink>
+        <NuxtLink to="/blog" class="no-underline font-semibold hover:bg-zinc-200 px-2 py-1 uppercase" activeClass="text-red-700">Blog</NuxtLink>
+      </div> 
         
         <!-- Mobile menu button -->
         
-<button type="button" class="md:hidden focus:outline-none max-w-max">
-            <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
-              <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
-            </svg>
-          </button>
-      
-          
- 
-  
-
+<button @click="toggleNav" type="button" class="w-8 h-8 md:hidden focus:outline-none">
+<div class="i-tabler-menu" />
+     </button>
     </nav>
-  </header>
+  </div>
 </template>
 
 <script>
-/*import { ref } from 'vue';
+    /*let showMenu = ref(false);
+    const toggleNav = () => (showMenu.value = !showMenu.value);*/
 export default {
-  setup() {
-    let showMenu = ref(false);
-    const toggleNav = () => (showMenu.value = !showMenu.value);
-    return { showMenu, toggleNav };
+  watch:{
+    '$route'(){
+      this.showMenu = false
+    }
   },
-};*/
-
-export default {
-    data() {
+  data() {
       return {
         showMenu: false,
       };
     },
-    methods: {
+  methods: {
       toggleNav: function (e) {
         this.showMenu = !this.showMenu;
       },
-
-    close (e) {
+      close (e) {
       if (!this.$el.contains(e.target)) {
         this.showMenu = false
       }
     }
   },
-  mounted () {
+  mounted() {
     document.addEventListener('click', this.close)
   },
-  beforeDestroy () {
+  beforeDestroy() {
     document.removeEventListener('click',this.close)
   }
 }
