540d4cf46febd03d3c781a9480de4c77ebe09d91
zhuganglie <pyrrhonianpig@gmail.com>
2022-09-23 14:11:20 +0800

    update

--- pages/about.vue
+++ pages/about.vue
@@ -2,6 +2,6 @@
 <div>
     <h1>About</h1>
     <hr />
-    <nuxt-img src="~/public/img/phil.jpeg" loading="lazy" width="550" />
+    <nuxt-img src="/img/phil.jpeg" loading="lazy" width="550" />
 </div>
 </template>
