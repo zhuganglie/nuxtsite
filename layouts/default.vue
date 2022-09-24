67597b77e74ef347275fa77b8950cb0c73feee0f
zhuganglie <pyrrhonianpig@gmail.com>
2022-09-24 15:51:07 +0800

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
-<div class="">
+<div class="flex flex-col h-screen justify-between ">
     <TheHeader />
-    <main class="px-4 md:w-2/3 mx-auto min-h-screen">
+    <main class="p-4 md:w-2/3 mx-auto mt-8 mb-auto">
     <slot />
   </main>
     <TheFooter />
