35bea87b9df4aaa0e053198e88c140d614fbd0df
zhuganglie <pyrrhonianpig@gmail.com>
2022-10-01 11:54:28 +0800

    add splide

--- pages/about.vue
+++ pages/about.vue
@@ -1,4 +1,26 @@
 
+
+<script>
+import { Splide, SplideSlide } from '@splidejs/vue-splide';
+import { defineComponent } from 'vue';
+import '@splidejs/vue-splide/css';
+
+export default defineComponent( {
+  components: {
+    Splide,
+    SplideSlide,
+  },
+  setup() {
+    const options = {
+      rewind: true,
+      gap   : '1rem',
+      height:'24em',
+    };
+    return { options };
+  },
+} );
+</script>
+
 <template>
 <NuxtLayout>
 <main>
@@ -8,6 +30,17 @@
 <article class="mt-8">
    <p>This is how I deal with life issues.</p> 
     <img src="/img/phil.jpeg" loading="lazy" width="550" class="max-w-[550px] w-full h-auto my-20 mx-auto block" />
+    <div>
+    <p>Here is what I like to do.</p>
+    <Splide :options="options" aria-label="My Favorite Images" class="w-full md:w-2/3 mx-auto">
+    <SplideSlide class="grid place-items-center place-content-center">
+      <div class="grid place-items-center place-content-center">Hello</div>
+    </SplideSlide>
+    <SplideSlide class="grid place-items-center place-content-center">
+      <div class="grid place-items-center place-content-center">That is great!</div>
+    </SplideSlide>
+  </Splide>
+  </div>
     </article>
 <section>
     <p>If you also like this way of thinking, drop me a line below.</p>
