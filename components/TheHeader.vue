c1f9b2ef5b65236a35fb4cf298a5a879be296ef0
zhuganglie <pyrrhonianpig@gmail.com>
2022-10-08 18:55:44 +0800

    update

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -6,14 +6,14 @@
     <router-link to="/" class=" text-2xl font-bold md:text-3xl no-underline"><span class="rounded-full border-2 px-3 md:px-3.5 py-0.5 bg-teal-900 text-teal-50">Z</span></router-link>
    
       <div :class="showMenu ? 'flex flex-col' : 'hidden'" class=" list-none mt-18 space-y-4  md:space-y-0 md:flex md:flex-row md:items-center md:space-x-10 md:m-0">
-        <NuxtLink  class="no-underline hover:bg-zinc-200 px-2 py-1 " activeClass="bg-zinc-200 font-bold" to="/">Home</NuxtLink>
+        <NuxtLink class="no-underline hover:bg-zinc-200 px-2 py-1 " activeClass="bg-zinc-200 font-bold" to="/">Home</NuxtLink>
         <NuxtLink class="no-underline hover:bg-zinc-200 px-2 py-1 " activeClass="bg-zinc-200 font-bold" to="/blog" >Blog</NuxtLink>
         <NuxtLink to="/tags" class="no-underline hover:bg-zinc-200 px-2 py-1 " exactActiveClass="bg-zinc-200 font-bold">Tags</NuxtLink>
         <NuxtLink to="/about" class="no-underline hover:bg-zinc-200 px-2 py-1 " activeClass="bg-zinc-200 font-bold">About</NuxtLink>
       </div> 
 
  <div @click="toggleNav" type="button" class="md:hidden" >        
- <div :class="showMenu ? 'i-tabler-align-left': 'i-tabler-align-right'" class="i-tabler-align-left text-teal-900 text-3xl"  />
+ <div :class="showMenu ? 'i-mdi-close': 'i-mdi-menu'" class=" text-teal-900 text-3xl"  />
   </div>   
     </nav>
   </div>
@@ -22,6 +22,9 @@
 
 <script>
   export default {
+  setup(){
+    const $route = useRoute();
+  },
   watch:{
     '$route'(){
       this.showMenu = false
