083f469664b0f53e24fff628aa3fb0a361fc9a3b
zhuganglie <pyrrhonianpig@gmail.com>
2022-11-17 18:01:43 +0800

    upgrade to v3.0.0

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import transformerDirective from '@unocss/transformer-directives'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 css: ["@/assets/styles/global.css"],
-buildModules:[
+modules:[
     '@unocss/nuxt',
     '@nuxt/content',
   //  '@formkit/nuxt',
