d2d7d660f01590ee31098551ee06871133ea97aa
zhuganglie <pyrrhonianpig@gmail.com>
2023-03-02 18:53:32 +0800

    update

--- components/content/PostHeader.vue
+++ components/content/PostHeader.vue
@@ -8,7 +8,7 @@ const { data } = await useAsyncData(`content-${path}`, () => queryContent().wher
     <h2 class="mb-4 leading-tight">
       {{ data.title }}
     </h2>
-    <time class="flex items-start mb-4 gap-x-2"><div class="i-tabler-calendar"/><span>{{ data.date.slice(0, 10) }}</span></time>
+    <time class="flex items-center mb-4 gap-x-2"><div class="i-tabler-calendar"/><span>{{ data.date.slice(0, 10) }}</span></time>
     <div class="flex gap-4">
     <span class="bg-zinc-200 px-2 py-0.5 rounded-full" v-bind:key="tag" v-for="tag in data.tags">
     <nuxt-link :to="`/tags/${tag}`" class="no-underline"> {{ tag }} </nuxt-link>
